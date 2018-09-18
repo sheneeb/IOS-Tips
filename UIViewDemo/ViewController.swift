ce43fd6a551bbaeaa84b4c5378a1bcceca4ad44b
Steve Lipton <steve@makeapppie.com>
2018-09-18 06:06:20 -0500

    Initial Commit

---
+++ UIViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewDemo
+//
+//  Created by Steven Lipton on 9/18/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
